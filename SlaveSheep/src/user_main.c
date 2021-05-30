c970c2cc363e2a6eddfe3f1bc3f4802508cb6fdd
Furkan Kalabalik <furkankalalabalikk@gmail.com>
2021-05-30 16:52:10 +0300

    commit

--- SlaveSheep/src/user_main.c
+++ SlaveSheep/src/user_main.c
@@ -4,6 +4,7 @@
 #include "mem.h"
 
 os_timer_t checktimer_wifestate;//Timer structure
+os_timer_t send_timer;
 struct espconn user_tcp_conn;//tcp structure
 
 uint32 ICACHE_FLASH_ATTR
@@ -63,6 +64,12 @@ void ICACHE_FLASH_ATTR user_tcp_recon_cb(void *arg,sint8 err)//tcp abnormal disc
 	os_printf("Connection error, error code: %d",err);
 	espconn_connect((struct espconn *)arg);//reconnect
 }
+
+void ICACHE_FLASH_ATTR send_data(void)
+{
+	espconn_send(&user_tcp_conn, "Hello from slave sheep", os_strlen("Hello from slave sheep"));
+}
+
 void ICACHE_FLASH_ATTR user_tcp_connent_cb(void *arg)//tcp connection establishment success callback function
 {
 	struct espconn *pespconn = arg;
@@ -70,7 +77,9 @@ void ICACHE_FLASH_ATTR user_tcp_connent_cb(void *arg)//tcp connection establishm
 	espconn_regist_recvcb(pespconn,user_tcp_recv_cb);//Register and receive a successful callback function
 	espconn_regist_sentcb(pespconn,user_tcp_sent_cb);//Register and send successfully callback function
 	espconn_regist_disconcb(pespconn,user_tcp_discon_cb);//Register the normal disconnection callback function
-//	espconn_send(pespconn,"8266",strlen("8266"));//
+    os_timer_disarm(&send_timer);//Cancel timer timing
+	os_timer_setfn(&send_timer,(os_timer_func_t *)send_data,NULL);//Set the timer callback function
+	os_timer_arm(&send_timer,5000,1);//Turn on the software timer for 500ms
 }
 
 void ICACHE_FLASH_ATTR my_station_init(struct ip_addr *remote_ip,
@@ -87,7 +96,7 @@ void ICACHE_FLASH_ATTR my_station_init(struct ip_addr *remote_ip,
 	//Enable connection
 	espconn_connect(&user_tcp_conn);
 }
-void check_WIFIstate(void)
+void ICACHE_FLASH_ATTR check_WIFIstate(void)
 {
 	uint8 getstate;//Define the connection state variable
 	getstate=wifi_station_get_connect_status();//Get sta connection status
@@ -96,17 +105,18 @@ void check_WIFIstate(void)
 		os_printf("WIFI connection is successful!");
 		os_timer_disarm(&checktimer_wifestate);//Turn off the software timer
 		struct ip_info info;
-		const char remote_ip[4]={192,168,1,186};//tcp server ip address, the server must first use this ip to open
+		const char remote_ip[4]={192,168,4,1};//tcp server ip address, the server must first use this ip to open
 		wifi_get_ip_info(STATION_IF,&info);//Query the ip of the module station
 		my_station_init((struct ip_addr *)remote_ip,&info.ip,8000);//Connect to port 8000 of the target server
 	}
 }
-void tcp_client_init()//tcp cilent initialization
+
+void ICACHE_FLASH_ATTR tcp_client_init()//tcp cilent initialization
 {
 	wifi_set_opmode(0x01);//Set to station mode to save flash
 	struct station_config stationconf;//stationa structure
-	os_strcpy(stationconf.ssid,"ZyXEL_9511_plus");//Set the username of the incoming route
-	os_strcpy(stationconf.password,"Furkan.1997");//Set the password to connect to the router
+	os_strcpy(stationconf.ssid,"MasterSheep");//Set the username of the incoming route
+	os_strcpy(stationconf.password,"masterpass");//Set the password to connect to the router
 	wifi_station_set_config(&stationconf);//Set the wifi interface configuration and save it to flash
 	wifi_station_connect();//Connect to the router
 	os_timer_disarm(&checktimer_wifestate);//Cancel timer timing
